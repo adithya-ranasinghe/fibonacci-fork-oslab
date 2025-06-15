3a6d8d45d57cb3e81b88fda99875c7a413006c8b
adithyaranasinghe08 <148994438+adithyaranasinghe08@users.noreply.github.com>
2025-06-15 15:11:38 +0530

    Add files via upload

---
+++ fibonacci_fork.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void generate_fibonacci(int n) {
+    int first = 0, second = 1, next;
+    for(int i = 0; i < n; i++) {
+        if(i <= 1)
+            next = i;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        printf("Usage: %s <number>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    if(n < 0) {
+        printf("Please enter a non-negative number.\n");
+        return 1;
+    }
+
+    pid_t pid = fork();
+
+    if(pid < 0) {
+        perror("Fork failed");
+        return 1;
+    } else if(pid == 0) {
+        printf("Child Process: Generating Fibonacci sequence of %d terms\n", n);
+        generate_fibonacci(n);
+        printf("Child Process ID: %d\n", getpid());
+    } else {
+        wait(NULL);
+        printf("Parent Process: Child process has completed.\n");
+    }
+
+    return 0;
+}
